71668b1d5e5ccc625b918c44504245908ffe116f
Recruitment <prabinkumar1999@outlook.com>
2022-06-15 14:34:48 +0400

    Login screen created

---
+++ src/pages/Login/index.jsx
@@ -0,0 +1,32 @@
+import React from 'react'
+import { UserOutlined, EyeTwoTone,  EyeInvisibleOutlined} from '@ant-design/icons';
+import { Input, Button } from 'antd';
+import ojimage from "../../images/oj.png"
+import "./Login.css"
+
+const Login = () => {
+  return (
+    <div className='body'>
+      <div className='login-body'>
+        <img className='oj-image' src={ojimage}/>
+        <p className='oj-title'>Log in to CV Parser</p>
+        <div className='name-bodies'>
+          <p className='name-title'>Email</p>
+          <Input size='large' type={"email"} placeholder={"Enter your email"} prefix={<UserOutlined />}></Input>
+        </div>
+        <div className='name-bodies'>
+          <p className='name-title'>Password</p>
+          <Input.Password
+          size='large'
+          placeholder="Enter your password"
+          prefix={<UserOutlined />}
+          iconRender={(visible) => (visible ? <EyeTwoTone /> : <EyeInvisibleOutlined />)}
+          />
+        </div>
+        <Button type='primary' className='button-primary' size='large'>Login</Button>
+      </div>
+    </div>
+  )
+}
+
+export default Login;
---
+++ src/pages/Register/index.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Register = () => {
+  return (
+    <>
+    <div>Register</div>
+    <p>This is a Register Page for registration</p>
+    </>
+  )
+}
+
+export default Register;
