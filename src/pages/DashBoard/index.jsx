b3feed4cf558ba48bef2226ecf9a25779bd61d4f
Recruitment <prabinkumar1999@outlook.com>
2022-06-15 18:36:51 +0400

    Login Page updated 15/06/2022 6:36pm

--- src/pages/DashBoard/index.jsx
+++ src/pages/DashBoard/index.jsx
@@ -1,10 +1,13 @@
-import React from "react";
+import React, { useState } from "react";
 import ojimage from "../../images/oj.png";
 import { RiUserSearchLine } from "react-icons/ri";
 import { AiOutlineUsergroupAdd } from "react-icons/ai";
 import "./DashBoard.css";
 
 const DashBoard = () => {
+  const [hoverStateCard1, setHoverStateCard1] = useState("");
+  const [hoverStateCard2, setHoverStateCard2] = useState("");
+
   return (
     <div className="body">
       <img className="" src={ojimage} alt={"Oman Jobs"} />
@@ -14,20 +17,28 @@ const DashBoard = () => {
           <h1 className="text-grey">Admin!</h1>
         </span>
         <div className="main-card">
-          <div className="card">
+          <div
+            className="card"
+            onMouseEnter={() => setHoverStateCard1("hover")}
+            onMouseLeave={() => setHoverStateCard1("")}
+          >
             <RiUserSearchLine className="card-icon" />
-            <h2 className="bolder">Search CV</h2>
-            <p className="text-grey">
+            <h2 className={`bolder ${hoverStateCard1}`}>Search CV</h2>
+            <p className={hoverStateCard1 ? hoverStateCard1 : "text-grey"}>
               Here, You can view all the CV with profile details.
             </p>
             <div className="go-corner" href="#">
               <div className="go-arrow">→</div>
             </div>
           </div>
-          <div className="card">
+          <div
+            className="card"
+            onMouseEnter={() => setHoverStateCard2("hover")}
+            onMouseLeave={() => setHoverStateCard2("")}
+          >
             <AiOutlineUsergroupAdd className="card-icon" />
-            <h2 className="bolder">Build CV</h2>
-            <p className="text-grey">
+            <h2 className={`bolder ${hoverStateCard2}`}>Build CV</h2>
+            <p className={hoverStateCard2 ? hoverStateCard2 : "text-grey"}>
               Here you can Create CV for Jobseeker or Modify the CV.
             </p>
             <div className="go-corner" href="#">
