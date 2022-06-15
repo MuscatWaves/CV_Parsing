74863c86ec5be4660febe37bbd4f4883a8a47a52
Recruitment <prabinkumar1999@outlook.com>
2022-06-15 15:52:58 +0400

    Login Page updated 15/06/2022 3:52pm

--- src/pages/Login/index.jsx
+++ src/pages/Login/index.jsx
@@ -1,33 +1,64 @@
-import React from 'react'
-import { UserOutlined, EyeTwoTone,  EyeInvisibleOutlined} from '@ant-design/icons';
-import { Input, Button } from 'antd';
-import ojimage from "../../images/oj.png"
-import "./Login.css"
-import {RiLockPasswordLine} from "react-icons/ri"
+import React, { useState } from "react";
+import {
+  UserOutlined,
+  EyeTwoTone,
+  EyeInvisibleOutlined,
+} from "@ant-design/icons";
+import { Input, Button } from "antd";
+import ojimage from "../../images/oj.png";
+import "./Login.css";
+import { RiLockPasswordLine } from "react-icons/ri";
 
 const Login = () => {
+  const [userName, setUserName] = useState("");
+  const [userPassword, setUserPassword] = useState("");
+  const [turnOn, setTurnOn] = useState(false);
+
   return (
-    <div className='body'>
-      <div className='login-body'>
-        <img className='oj-image' src={ojimage}/>
-        <p className='oj-title'>Log in to CV Parser</p>
-        <div className='name-bodies'>
-          <p className='name-title'>Email</p>
-          <Input size='large' type={"email"} placeholder={"Enter your email"} prefix={<UserOutlined />}></Input>
+    <div className="body">
+      <div className="login-body">
+        <img className="oj-image" src={ojimage} />
+        <p className="oj-title">Log in to CV Parser</p>
+        <div className="name-bodies">
+          <p className="name-title">Email</p>
+          <Input
+            className="input-primary"
+            size="large"
+            type={"email"}
+            placeholder={"Enter your email"}
+            value={userName}
+            onChange={(e) => setUserName(e.target.value)}
+            prefix={<UserOutlined />}
+          ></Input>
         </div>
-        <div className='name-bodies'>
-          <p className='name-title'>Password</p>
+        <div className="name-bodies">
+          <p className="name-title">Password</p>
           <Input.Password
-          size='large'
-          placeholder="Enter your password"
-          prefix={<RiLockPasswordLine />}
-          iconRender={(visible) => (visible ? <EyeTwoTone /> : <EyeInvisibleOutlined />)}
+            className="input-primary"
+            size="large"
+            placeholder="Enter your password"
+            prefix={<RiLockPasswordLine />}
+            iconRender={(visible) =>
+              visible ? <EyeTwoTone /> : <EyeInvisibleOutlined />
+            }
+            onChange={(e) => setUserPassword(e.target.value)}
           />
         </div>
-        <Button type='primary' className='button-primary' size='large'>Login</Button>
+        <Button
+          type="primary"
+          className="button-primary"
+          size="large"
+          loading={turnOn}
+          onClick={() => {
+            console.log(userName, userPassword);
+            setTurnOn(!turnOn);
+          }}
+        >
+          Login
+        </Button>
       </div>
     </div>
-  )
-}
+  );
+};
 
 export default Login;
