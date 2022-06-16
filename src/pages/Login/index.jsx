c8488f17ba3d2214d823279c4c55c3d82035fa07
Recruitment <prabinkumar1999@outlook.com>
2022-06-16 08:59:20 +0400

    Login page updated 16/06/2022 9am

--- src/pages/Login/index.jsx
+++ src/pages/Login/index.jsx
@@ -1,64 +1,76 @@
 import React, { useState } from "react";
-import {
-  UserOutlined,
-  EyeTwoTone,
-  EyeInvisibleOutlined,
-} from "@ant-design/icons";
-import { Input, Button } from "antd";
+import { UserOutlined, LockOutlined } from "@ant-design/icons";
+import { Input, Button, Form } from "antd";
 import ojimage from "../../images/oj.png";
 import "./Login.css";
-import { RiLockPasswordLine } from "react-icons/ri";
 
 const Login = () => {
-  const [userName, setUserName] = useState("");
-  const [userPassword, setUserPassword] = useState("");
   const [turnOn, setTurnOn] = useState(false);
+
+  const handleSubmit = (values) => {
+    console.log(values);
+    setTurnOn(!turnOn);
+  };
 
   return (
     <div className="body">
-      <div className="login-body">
+      <Form
+        name="normal_login"
+        className="login-form"
+        initialValues={{
+          remember: true,
+        }}
+        onFinish={handleSubmit}
+      >
         <img className="oj-image" src={ojimage} alt={"Oman Jobs"} />
         <p className="oj-title">Log in to CV Parser</p>
         <div className="name-bodies">
           <p className="name-title">Email</p>
-          <Input
+          <Form.Item
             className="input-primary"
-            size="large"
-            type={"email"}
-            placeholder={"Enter your email"}
-            value={userName}
-            onChange={(e) => setUserName(e.target.value)}
-            prefix={<UserOutlined />}
-            style={{ fontWeight: "600" }}
-          ></Input>
+            name="username"
+            rules={[
+              {
+                required: true,
+                message: "Please enter your Username!",
+              },
+            ]}
+          >
+            <Input
+              prefix={<UserOutlined className="site-form-item-icon" />}
+              placeholder="Username"
+            />
+          </Form.Item>
         </div>
         <div className="name-bodies">
           <p className="name-title">Password</p>
-          <Input
+          <Form.Item
             className="input-primary"
-            type={"password"}
-            size="large"
-            placeholder="Enter your password"
-            prefix={<RiLockPasswordLine />}
-            iconRender={(visible) =>
-              visible ? <EyeTwoTone /> : <EyeInvisibleOutlined />
-            }
-            onChange={(e) => setUserPassword(e.target.value)}
-          />
+            name="password"
+            rules={[
+              {
+                required: true,
+                message: "Please enter your Password!",
+              },
+            ]}
+          >
+            <Input
+              prefix={<LockOutlined className="site-form-item-icon" />}
+              type="password"
+              placeholder="Password"
+            />
+          </Form.Item>
         </div>
         <Button
-          type="submit"
+          type="primary"
+          htmlType="submit"
           className="button-primary"
           size="large"
           loading={turnOn}
-          onClick={() => {
-            console.log(userName, userPassword);
-            setTurnOn(!turnOn);
-          }}
         >
           Login
         </Button>
-      </div>
+      </Form>
     </div>
   );
 };
