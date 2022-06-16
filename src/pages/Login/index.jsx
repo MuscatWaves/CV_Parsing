6252d8939635efe2d0f5be54874df54f9de9ddc4
salimsa7 <salimakhtars4@gmail.com>
2022-06-16 15:39:26 +0530

    middleware

--- src/pages/Login/index.jsx
+++ src/pages/Login/index.jsx
@@ -1,15 +1,66 @@
 import React, { useState } from "react";
 import { UserOutlined, LockOutlined } from "@ant-design/icons";
-import { Input, Button, Form } from "antd";
+import { Input, Button, Form, message } from "antd";
 import ojimage from "../../images/oj.png";
 import "./Login.css";
+import FormData from "form-data";
+import isEmail from "validator/lib/isEmail";
+import isStrongPassword from "validator/lib/isStrongPassword";
+import axios from "axios";
 
 const Login = () => {
-  const [turnOn, setTurnOn] = useState(false);
+  const [isLoading, setLoading] = useState(false);
 
-  const handleSubmit = (values) => {
-    console.log(values);
-    setTurnOn(!turnOn);
+  const handleSubmit = async (values) => {
+    const Email = values["email"];
+    const Password = values["password"];
+    setLoading(true);
+    if (!isEmail(Email)) {
+      message.error("Please Enter a valid Email");
+      setLoading(false);
+      return;
+    }
+
+    if (!isStrongPassword(Password)) {
+      message.error(
+        "Password must contains one upper case, one lower case, one numeric digit, minimum 8 characters, one special character"
+      );
+      setLoading(false);
+      return;
+    }
+
+    var bodyFormData = new FormData();
+    bodyFormData.append("email", Email);
+    bodyFormData.append("password", Password);
+    bodyFormData.append("Login", Password);
+    await axios({
+      method: "POST",
+      url: `/api/login.php`,
+      data: bodyFormData,
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "multipart/form-data",
+      },
+    })
+      .then(function (response) {
+        console.log(response);
+        if (response.status === 200 && response.data.token) {
+          setLoading(false);
+          message.error("Login Successfull, Redirecting...", "success");
+        } else {
+          if (response.status === 201) {
+            setLoading(false);
+            message.error(response.data.error, "error");
+          } else {
+            setLoading(false);
+            message.error("Something Went Wrong!", "error");
+          }
+        }
+      })
+      .catch(function (response) {
+        setLoading(false);
+        message.error("Something Went Wrong!", "error");
+      });
   };
 
   return (
@@ -28,17 +79,17 @@ const Login = () => {
           <p className="name-title">Email</p>
           <Form.Item
             className="input-primary"
-            name="username"
+            name="email"
             rules={[
               {
                 required: true,
-                message: "Please enter your Username!",
+                message: "Please enter your Email!",
               },
             ]}
           >
             <Input
               prefix={<UserOutlined className="site-form-item-icon" />}
-              placeholder="Username"
+              placeholder="Email"
             />
           </Form.Item>
         </div>
@@ -66,7 +117,7 @@ const Login = () => {
           htmlType="submit"
           className="button-primary"
           size="large"
-          loading={turnOn}
+          loading={isLoading}
         >
           Login
         </Button>
