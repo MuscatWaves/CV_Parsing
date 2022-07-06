3787c78e83628c9c562c4f12f8f8422d1b6f39f2
Recruitment <prabinkumar1999@outlook.com>
2022-07-06 14:16:58 +0400

    page updated

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import "./app.css";
 import Routing from "./routes";
+import { HelmetProvider } from "react-helmet-async";
 
 function App() {
   return (
     <div className="App">
+      <HelmetProvider>
         <header className="App-header">
           <Routing/>
         </header>
+      </HelmetProvider>
     </div>
   );
 }
