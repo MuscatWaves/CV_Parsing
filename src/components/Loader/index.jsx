827d1b9a387c6d17f3fc73cea287756d9bdc6ffa
Recruitment <prabinkumar1999@outlook.com>
2022-07-06 14:07:43 +0400

    app updated

--- src/components/Loader/index.jsx
+++ src/components/Loader/index.jsx
@@ -2,7 +2,6 @@ import { Spin } from "antd";
 import React, { useEffect } from "react";
 import ojimage from "../../images/oj.png";
 import "./loader.css";
-import { Helmet } from "react-helmet";
 
 const Loader = ({ minHeight }) => {
   useEffect(() => {
@@ -11,9 +10,6 @@ const Loader = ({ minHeight }) => {
 
   return (
     <div className="loading-data" style={{ minHeight: minHeight }}>
-      <Helmet>
-        <title>Loading</title>
-      </Helmet>
       <div className="inner-loading-data">
         <img src={ojimage} className="loader-image" alt="Oman jobs" />
         <Spin size="large" />
