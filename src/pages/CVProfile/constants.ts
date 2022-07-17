6c0655d72fbbe712ae6365617f3e004a20810c54
Prabin Kumar <prabinkumar@Prabins-MacBook-Air.local>
2022-07-17 23:24:16 +0400

    dash upd

--- src/pages/CVProfile/constants.ts
+++ src/pages/CVProfile/constants.ts
@@ -8,3 +8,37 @@ export const categorySelection =  [
     {label: "Attested Certificate", value: 6},
     {label: "Reference Check", value: 7},
 ]
+
+export const education_dummy = [
+    {
+        edu_name: "Btech",
+        college: "Carmel College of Engineering & Technology",
+        edu_loc: "Alappuzha, Kerala",
+        edu_from: "2017",
+        edu_to: "2022"
+    },
+    {
+        edu_name: "12th",
+        college: "Seventh Day Adventist English High School",
+        edu_loc: "Alappuzha, Kerala",
+        edu_from: "2013",
+        edu_to: "2017"
+    }
+]
+
+export const work_experience_dummy = [
+    {
+        "ex_name": "SeeMyMachines",
+        "desg": "Junior Software Developer",
+        "ex_from": "2021",
+        "ex_to": "2022",
+        "desc": "- Worked at so many things\n- Developed Main Website\n- Developed Sub Website\n- and many more fun things"
+    },
+    {
+        "ex_name": "Oman Jobs",
+        "desg": "Full Stack Web Developer",
+        "ex_from": "2022",
+        "ex_to": "Present",
+        "desc": "Working at Muscat Waves\nKinda looking cool\nCV Parsing Application\nOman Jobs website\nTution Application"
+    }
+]
